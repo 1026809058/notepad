b43b8eb34c219e9fe7af297599c6e3c094481346
1026809058@qq.com <1026809058@qq.com>
2021-09-16 18:11:21 +0800

    五子棋

--- src/views/notepad/myStudy/canvas/canvas_02/canvas_02.vue
+++ src/views/notepad/myStudy/canvas/canvas_02/canvas_02.vue
@@ -20,60 +20,103 @@ export default defineComponent({
         let wHeight = canvas2.value.offsetHeight;
         let canvasSize = wHeight;
         //线条条数
-        let interval: number = wHeight / 25;
+        let interval: number = 0;
+        let unnecessaryX: number;
+        let unnecessaryY: number;
+        switch (canvasSize) {
+          case 301:
+            unnecessaryX = 210;
+            unnecessaryY = 270;
+            interval = wHeight / 25 - 3;
+            break;
+          case 601:
+            unnecessaryX = 500;
+            unnecessaryY = 560;
+            interval = wHeight / 25 - 5;
+            break;
+          case 721:
+            unnecessaryX = 620;
+            unnecessaryY = 680;
+            interval = wHeight / 25 - 5;
+            break;
+          default:
+            unnecessaryX = 0;
+            unnecessaryY = 0;
+            break;
+        }
         canvas.value.width = wWidth;
         canvas.value.height = wHeight;
         const ctx = canvas.value.getContext("2d");
         ctx.translate(0.5, 0.5);
-        for (let i = 0; i < interval; i++) {
+        for (let i = 1; i < interval; i++) {
           ctx.beginPath();
-          ctx.moveTo(0, 30 * i);
-          ctx.lineTo(canvasSize, 30 * i);
+          ctx.moveTo(20, 30 * i);
+          ctx.lineTo(canvasSize - 20, 30 * i);
           ctx.stroke();
           ctx.closePath();
         }
-        for (let i = 0; i < interval; i++) {
+        for (let i = 1; i < interval; i++) {
           ctx.beginPath();
-          ctx.moveTo(30 * i, 0);
-          ctx.lineTo(30 * i, canvasSize);
+          ctx.moveTo(30 * i, 20);
+          ctx.lineTo(30 * i, canvasSize - 20);
           ctx.stroke();
           ctx.closePath();
         }
         //是否黑棋
         let isBlack = true;
         //棋盘二维数组
-        let cheeks: number[] = [];
-        let arrLength = parseInt(interval);
-        for (let i = 0; i < interval; i++) {
+        let cheeks: any[] = [];
+        let arrLength = parseInt(`${interval}`);
+        for (let i = 0; i < interval - 1; i++) {
           cheeks[i] = new Array(arrLength).fill(0);
         }
         console.log("cheeks", cheeks);
         //创建棋子点击事件
         canvas.value.onclick = function (e: any) {
-          const clientX = e.clientX;
-          const clientY = e.clientY;
-          console.log("clientY", clientX, clientY);
-          //取整，确保棋子落在交叉点
-          const x = Math.round(clientX / 30) * 30 + 20;
-          const y = Math.round(clientY / 30) * 30 + 20;
-          console.log("x", x, y);
-          // cheeks二维数组的索引
-          const cheeksX = (x - 20) / 30;
-          const cheeksY = (y - 20) / 30;
-          console.log(cheeksX, cheeksY);
-          // 对应元素不为0说明此地方已有棋，返回
-          if (cheeks[cheeksY][cheeksX]) return;
-          // 黑棋为1，白棋为2
-          cheeks[cheeksY][cheeksX] = isBlack ? 1 : 2;
-          ctx.beginPath();
-          // 画圆
-          ctx.arc(x, y, 20, 0, 2 * Math.PI);
-          // 判断走黑还是白
-          ctx.fillStyle = isBlack ? "black" : "white";
-          ctx.fill();
-          ctx.closePath();
-          // 切换黑白
-          isBlack = !isBlack;
+          try {
+            const clientX = e.clientX;
+            const clientY = e.clientY;
+            console.log("clientY", clientX, clientY);
+            console.log(canvasSize);
+            //取整，确保棋子落在交叉点
+            const x = Math.round(clientX / 30) * 30 - unnecessaryX + 30;
+            let y = 0;
+            if (clientY % 30 < 4) {
+              y = Math.round(clientY / 30) * 30 - unnecessaryY + 30;
+              console.log("y", y, clientY / 30);
+            } else {
+              y = (Math.round(clientY / 30) - 1) * 30 - unnecessaryY + 30;
+              console.log("y", y, clientY / 30);
+            }
+            console.log("x", x, y, clientY % 30);
+            // cheeks二维数组的索引
+            const cheeksX = x / 30 - 1;
+            const cheeksY = y / 30 - 1;
+            console.log(cheeksX, cheeksY);
+            // 对应元素不为0说明此地方已有棋，返回
+            if (
+              cheeks[cheeksY][cheeksX] ||
+              cheeksY < 0 ||
+              cheeksX < 0 ||
+              cheeksX > interval - 1 ||
+              cheeksY > interval - 1
+            )
+              return;
+            // 黑棋为1，白棋为2
+            cheeks[cheeksY][cheeksX] = isBlack ? 1 : 2;
+            ctx.beginPath();
+            // 画圆
+            ctx.arc(x + 0.5, y + 0.5, 15, 0, 2 * Math.PI);
+            // 判断白棋走还是黑棋，填充色
+            ctx.fillStyle = isBlack ? "black" : "white";
+            //填充
+            ctx.fill();
+            ctx.closePath();
+            // 切换黑白
+            isBlack = !isBlack;
+          } catch (e) {
+            console.log(e);
+          }
         };
       }
       chess();
@@ -112,7 +155,7 @@ export default defineComponent({
   }
 }
 @media screen and (min-width: 70em) {
-  $line: 811px;
+  $line: 721px;
   .chessboard {
     width: $line;
     min-height: $line;
